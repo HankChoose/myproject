240932284b94b7cc7484f95e195e727bfda1f029
HankChoose <hankchenv@gmail.com>
2023-08-18 22:25:52 -0600

    Update models.py

--- backend/django_project/models.py
+++ backend/django_project/models.py
@@ -3,8 +3,7 @@ from django.db import models
 
 class User(models.Model):
     email = models.EmailField(unique=True)
-    password = models.CharField(max_length=100)
-    created_at = models.DateTimeField(auto_now_add=True)
+    password = models.CharField(max_length=128)
 
     def __str__(self):
-        return self.username
+        return self.email
