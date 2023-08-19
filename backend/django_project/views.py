84fb41c7bfd69914013ee233245c3c1453d01635
HankChoose <hankchenv@gmail.com>
2023-08-19 02:01:04 -0600

    Update views.py

--- backend/django_project/views.py
+++ backend/django_project/views.py
@@ -2,7 +2,6 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from .models import User
-from django.contrib.auth.models import User
 
 from .serializers import UserSerializer
 from django.http import HttpResponseServerError
