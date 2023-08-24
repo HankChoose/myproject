9f468578a9a7b19e637f02b787b2a1bf4a02a69f
HankChoose <hankchenv@gmail.com>
2023-08-24 01:11:29 -0600

    Update settings.py

--- backend/django_project/settings.py
+++ backend/django_project/settings.py
@@ -168,6 +168,7 @@ TEMPLATES = [
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [],
         # 'DIRS': [os.path.join(BASE_DIR, 'frontend', 'build')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],  # 您自己的模板文件夹路径
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -179,6 +180,10 @@ TEMPLATES = [
         },
     },
 ]
+
+# 配置 allauth 使用的模板
+ACCOUNT_SIGNUP_TEMPLATE = 'account/signup.html'
+ACCOUNT_LOGIN_TEMPLATE = 'account/login.html'
 
 WSGI_APPLICATION = 'django_project.wsgi.application'
 
