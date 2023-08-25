f18439db89649a1397bc1d764dcade7157080a09
HankChoose <hankchenv@gmail.com>
2023-08-24 22:41:57 -0600

    Update api.js

--- frontend/react_app/src/api.js
+++ frontend/react_app/src/api.js
@@ -2,23 +2,25 @@ import axios from 'axios';
 import BASE_URL from './api-config'; // 导入之前定义的基本URL
 import Cookies from 'js-cookie';
 
-const csrfToken = Cookies.get('csrftoken'); // 获取 CSRF token
+//const csrfToken = Cookies.get('csrftoken'); // 获取 CSRF token
+
+const csrfToken = document.cookie
+  .split('; ')
+  .find(cookie => cookie.startsWith('csrftoken='))
+  .split('=')[1];
+
+// 设置CSRF令牌到请求头部
+axios.defaults.headers.common['X-CSRFToken'] = csrfToken;
+
 const login = async (username, password) => {
 
   try {
-    const csrfToken = Cookies.get('csrftoken'); // 获取 CSRF token
-    // 设置CSRF令牌作为请求头
-    const config = {
-      headers: {
-        'Content-Type': 'application/json',
-        'X-CSRFToken': csrfToken, // 你的CSRF令牌的名称可能不同
-      },
-    };
+    
     //const response = await axios.post(`${BASE_URL}login/`, {
     const response = await axios.post('/accounts/login/', {
         username: username,
         password: password,
-    },config);
+    };
     return response.data;
   } catch (error) {
     throw error;
@@ -29,15 +31,9 @@ const login = async (username, password) => {
 const signup = async (userData) => {
   try {
     const csrfToken = Cookies.get('csrftoken'); // 获取 CSRF token
-    // 设置CSRF令牌作为请求头
-    const config = {
-      headers: {
-        'Content-Type': 'application/json',
-        'X-CSRFToken': csrfToken, // 你的CSRF令牌的名称可能不同
-      },
-    };
+    
     //const response = await axios.post(`${BASE_URL}register/`, userData);
-    const response = await axios.post('/accounts/signup/', userData,config);
+    const response = await axios.post('/accounts/signup/', userData);
     return response.data;
   } catch (error) {
     throw error;
