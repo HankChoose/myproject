66d9e209f8676e3deca86e9473fafc3d69960e4f
HankChoose <hankchenv@gmail.com>
2023-08-25 00:22:29 -0600

    Update api.js

--- frontend/react_app/src/api.js
+++ frontend/react_app/src/api.js
@@ -2,21 +2,28 @@ import axios from 'axios';
 import BASE_URL from './api-config'; // 导入之前定义的基本URL
 import Cookies from 'js-cookie';
 
- // 获取存储在 JavaScript 变量中的 CSRF 令牌
- const csrfToken = "{{ csrf_token }}";
+const csrfToken = Cookies.get('csrftoken'); // 获取 CSRF token
+
+// 获取CSRF令牌
+axios.get('/accounts/csrf/').then((response) => {
+  csrfToken = response.data.csrfToken;
+});
+
+const config = {
+  headers: {
+    'X-CSRFToken': csrfToken, // 你的CSRF令牌的名称可能不同
+  },
+};
 
 const login = async (email, password) => {
 
   try {
-   
-    // 设置 Axios 的默认请求头
-    axios.defaults.headers.common['X-CSRFToken'] = csrfToken;
-
+  
     //const response = await axios.post(`${BASE_URL}login/`, {
     const response = await axios.post('/accounts/login/', {
         email: email,
         password: password,
-    });
+    },config);
     return response.data;
   } catch (error) {
     throw error;
@@ -30,7 +37,7 @@ const signup = async (userData) => {
   axios.defaults.headers.common['X-CSRFToken'] = csrfToken;
   try {
     //const response = await axios.post(`${BASE_URL}register/`, userData);
-    const response = await axios.post('/accounts/signup/', userData);
+    const response = await axios.post('/accounts/signup/', userData,config);
     return response.data;
   } catch (error) {
     throw error;
