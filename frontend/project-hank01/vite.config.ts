525a9e3418ebcddbbbcc1e169a3c0c852d71ac3a
HankChoose <hankchenv@gmail.com>
2023-10-06 11:32:43 -0600

    Update vite.config.ts

--- frontend/project-hank01/vite.config.ts
+++ frontend/project-hank01/vite.config.ts
@@ -6,6 +6,7 @@ import svgr from 'vite-plugin-svgr';
 export default defineConfig({
     plugins: [react(), svgr()],
     server: {
+        host: '0.0.0.0', // 允许外部访问
         port: 3000 // 自定义端口号
     }
 });
