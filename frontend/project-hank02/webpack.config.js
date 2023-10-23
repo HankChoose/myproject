73834dfa0c2544f677a4393748591e613528f948
HankChoose <hankchenv@gmail.com>
2023-10-23 01:20:39 -0600

    3

--- frontend/project-hank02/webpack.config.js
+++ frontend/project-hank02/webpack.config.js
@@ -22,6 +22,22 @@ export default {
             },
         ],
     },
+	devServer: {
+      port: 3000, // 设置Webpack开发服务器监听的端口号
+      //host: '0.0.0.0', // 允许外部访问
+      allowedHosts: [
+        'zhiyouyuec.com', // 允许的主机名或域名
+        'localhost', // 本地主机
+      ],
+      
+    },
+	proxy: {
+	'/ws': {
+	  target: 'wss://zhiyouyuec.com', // WebSocket服务器的地址
+	  ws: true, // 启用WebSocket代理
+	  secure: false, // 如果WebSocket服务器使用自签名证书，请将其设置为false
+	},
+},
     resolve: {
         extensions: ['.ts', '.tsx', '.js', '.json'],
     },
