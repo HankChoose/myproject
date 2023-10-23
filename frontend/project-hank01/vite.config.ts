d76f1a809f0ab458ed3cba3842bc36f383e7aaae
HankChoose <hankchenv@gmail.com>
2023-10-22 22:30:19 -0600

    Update vite.config.ts

--- frontend/project-hank01/vite.config.ts
+++ frontend/project-hank01/vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
-import vue from '@vitejs/plugin-vue';
 import ViteSassPlugin from 'vite-plugin-sass'; // 导入Sass插件
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react(), svgr(),vue(), ViteSassPlugin()],
+    plugins: [react(), svgr(), ViteSassPlugin()],
 	
 });
