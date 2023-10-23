970d2af40c4e23e78fd6822e84d90aa1f977a005
HankChoose <hankchenv@gmail.com>
2023-10-22 21:56:27 -0600

    Update vite.config.ts

--- frontend/project-hank01/vite.config.ts
+++ frontend/project-hank01/vite.config.ts
@@ -5,5 +5,10 @@ import svgr from 'vite-plugin-svgr';
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react(), svgr()],
-	
+	css: {
+    preprocessorOptions: {
+      scss: {
+        // your options here
+      }
+    }
 });
