2825114c9bf53c58c1da5721ad7335e1cd03036f
HankChoose <hankchenv@gmail.com>
2023-10-22 21:01:40 -0600

    all-new4

--- frontend/project-hank01/src/_codux/boards/hanktest-2/hanktest-2.board.tsx
+++
@@ -1,8 +0,0 @@
-import { createBoard } from '@wixc3/react-board';
-import { Hanktest2 } from '../../../components/hanktest-2/hanktest-2';
-
-export default createBoard({
-    name: 'Hanktest2',
-    Board: () => <Hanktest2 />,
-    isSnippet: true,
-});
--- frontend/project-hank01/src/_codux/boards/register-form/register-form.board.tsx
+++
@@ -1,8 +0,0 @@
-import { createBoard } from '@wixc3/react-board';
-import { RegisterForm } from '../../../components/register-form/register-form';
-
-export default createBoard({
-    name: 'RegisterForm',
-    Board: () => <RegisterForm />,
-    isSnippet: true,
-});
