677793ae21837a93be9695d0a3fc968d88406fc5
HankChoose <hankchenv@gmail.com>
2023-10-25 23:14:03 -0600

    3

--- frontend/project-hank01/vite.config.ts
+++ frontend/project-hank01/vite.config.ts
@@ -9,12 +9,4 @@ export default defineConfig({
         host: '0.0.0.0', // 允许外部访问
         port: 3000,      // 选择一个可供外部访问的端口
     },
-    css: {
-    preprocessorOptions: {
-        scss: {
-            additionalData: '@import "@/styles/variables.scss";', // 导入全局变量
-            outputStyle: 'expanded', // 输出样式格式，可选值有 'expanded'、'compressed' 等
-        },
-    },
-    },
 });
