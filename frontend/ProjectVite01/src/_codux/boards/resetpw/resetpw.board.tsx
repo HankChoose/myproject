301fac3d876886cae2a55970bc5ec2b3f045a65d
HankChoose <hankchenv@gmail.com>
2023-10-29 15:48:53 -0600

    2

--- frontend/ProjectVite01/src/_codux/boards/resetpw/resetpw.board.tsx
+++
@@ -1,8 +0,0 @@
-import { createBoard } from '@wixc3/react-board';
-import { Resetpw } from '../../../components/resetpw/resetpw';
-
-export default createBoard({
-    name: 'Resetpw',
-    Board: () => <Resetpw />,
-    isSnippet: true,
-});
