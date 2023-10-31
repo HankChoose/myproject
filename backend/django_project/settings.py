6f4e34fc9ce9d1d5d91f06e95f2305f2ade2f1eb
HankChoose <hankchenv@gmail.com>
2023-10-31 03:42:12 -0600

    Update settings.py

--- backend/django_project/settings.py
+++ backend/django_project/settings.py
@@ -187,13 +187,14 @@ WSGI_APPLICATION = 'django_project.wsgi.application'
 # https://docs.djangoproject.com/en/dev/ref/settings/#databases
 
 
-# DATABASES = {
-# 'default': {
-# 'ENGINE': 'django.db.backends.sqlite3',
-# 'NAME': BASE_DIR / 'db.sqlite3',
-# }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
@@ -207,7 +208,7 @@ DATABASES = {
 
     }
 }
-
+'''
 
 # Password validation
 # https://docs.djangoproject.com/en/dev/ref/settings/#auth-password-validators
