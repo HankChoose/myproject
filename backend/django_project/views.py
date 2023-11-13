41e6dffd0cb0bee170c115adcded624c1b3d0d4f
HankChoose <hankchenv@gmail.com>
2023-11-13 11:05:37 -0700

    2

--- backend/django_project/views.py
+++ backend/django_project/views.py
@@ -27,14 +27,13 @@ from allauth.account.views import LoginView, SignupView, PasswordChangeView
 
 # ------------------------------------------------------------>Check exists email
 
-@csrf_exempt
+
 def check_email_exist(request, email):
     from django.contrib.auth.models import User
     exists = User.objects.filter(email=email).exists()
     return JsonResponse({'exists': exists})
 
 
-@csrf_exempt
 class CheckUserAPIView(APIView):
     def post(self, request, *args, **kwargs):
         email = request.data.get('email', '')
@@ -47,7 +46,6 @@ class CheckUserAPIView(APIView):
 # ------------------------------------------------------------>For UserDemand
 
 
-@csrf_exempt
 class UserDemandCreateView(generics.CreateAPIView):
     queryset = UserDemand.objects.all()
     serializer_class = UserDemandSerializer
