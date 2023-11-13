2f6b870873eace41bee94993c35262d7cacb2e66
HankChoose <hankchenv@gmail.com>
2023-11-13 15:08:12 -0700

    2

--- backend/django_project/views.py
+++ backend/django_project/views.py
@@ -2,7 +2,8 @@ from .models import User
 from .models import UserDemand
 
 import json
-
+from django.shortcuts import render
+from django.http import JsonResponse
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.http import HttpResponseServerError
