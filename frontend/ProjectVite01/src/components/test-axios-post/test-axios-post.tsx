ccc2f655dae6705a17462e8244e7ff8c24558157
HankChoose <hankchenv@gmail.com>
2023-11-14 13:04:04 -0700

    2

--- frontend/ProjectVite01/src/components/test-axios-post/test-axios-post.tsx
+++ frontend/ProjectVite01/src/components/test-axios-post/test-axios-post.tsx
@@ -38,7 +38,7 @@ export const TestAxiosPost = ({ className }: TestAxiosPostProps) => {
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
-    const apiUrl = '/accounts/signup/';
+    const apiUrl = '/send-data/';
     
     const userData = {
       username: 'hankchenv111@gmail.com', // 用户名
