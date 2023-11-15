a58749274bb1e92209460e04b990cf54cd1fa19e
HankChoose <hankchenv@gmail.com>
2023-11-15 00:30:20 -0700

    Update test-axios-post.tsx

--- frontend/ProjectVite01/src/components/test-axios-post/test-axios-post.tsx
+++ frontend/ProjectVite01/src/components/test-axios-post/test-axios-post.tsx
@@ -38,7 +38,7 @@ export const TestAxiosPost = ({ className }: TestAxiosPostProps) => {
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
-    const apiUrl = '/accounts/signup/';
+    const apiUrl = '/accounts/login/';
 
     const userData = {
       username: 'hank3', // 用户名
