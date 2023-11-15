0ad0a78dab511e600c58a0970fabce57f677fc50
HankChoose <hankchenv@gmail.com>
2023-11-14 21:58:04 -0700

    2

--- backend/django_project/urls.py
+++ backend/django_project/urls.py
@@ -2,13 +2,7 @@ from django.contrib import admin
 from django.urls import include, path
 from allauth.account.views import LoginView, LogoutView, SignupView, PasswordChangeView
 from . import views
-from .views import UserDemandCreateView
-from .views import CheckUserAPIView
-from .views import Receive_data
-from .views import Receive_data2
-from .views import Register
-from .views import Register2
-from .views import check_email_exist
+
 from django.views.decorators.csrf import csrf_exempt
 
 app_name = 'django_project'
@@ -16,16 +10,15 @@ app_name = 'django_project'
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", views.index, name="index"),
-    path('encyclopedia/', include("encyclopedia.urls")),
-    path('create/', UserDemandCreateView.as_view(), name='create-demand'),
-    path('receive_data/', Receive_data, name='receive_data'),
-    path('receive_data2/', Receive_data2, name='receive_data2'),
+    # path('encyclopedia/', include("encyclopedia.urls")),
+    path('create/', views.UserDemandCreateView, name='create-demand'),
+    path('receive_data/', views.Receive_data, name='receive_data'),
+    path('receive_data2/', views.Receive_data2, name='receive_data2'),
     path('send-data/', views.Register, name='register'),
     path('send-data2/', views.Register2, name='register2'),
     path('api/check-email-exist/<str:email>/',
-         check_email_exist, name='check_email_exist'),
-    path('api/check_user/', CheckUserAPIView.as_view(), name='check_user'),
-    path('accounts/', include('allauth.urls')),
+         views.check_email_exist, name='check_email_exist'),
+    path('api/check_user/', views.CheckUserAPIView, name='check_user'),
     path('accounts/login/', LoginView.as_view(), name='account_login'),
     path('accounts/logout/', LogoutView.as_view(),
          name='account_logout'),
