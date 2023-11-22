44af843954ebff0e65852a7fed11ae311e391063
HankChoose <hankchenv@gmail.com>
2023-11-22 12:26:41 -0700

    2

--- frontend/ProjectVite01/src/_codux/boards/user-profile/user-profile.board.tsx
+++ frontend/ProjectVite01/src/_codux/boards/user-profile/user-profile.board.tsx
@@ -1,8 +1,9 @@
 import { createBoard } from '@wixc3/react-board';
 import { UserProfile } from '../../../components/user-profile/user-profile';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 
 export default createBoard({
     name: 'UserProfile',
-    Board: () => <UserProfile />,
+    Board: () =><Router> <UserProfile /></Router>,
     isSnippet: true,
 });
