ab56a526d785c5d1886ebca7a73b9182f42528e1
HankChoose <hankchenv@gmail.com>
2023-11-23 01:00:25 -0700

    2

--- frontend/ProjectVite01/src/components/user-profile/user-profile.tsx
+++ frontend/ProjectVite01/src/components/user-profile/user-profile.tsx
@@ -1,14 +1,58 @@
 import classNames from 'classnames';
 import styles from './user-profile.module.scss';
+import axios from 'axios';
+import React, { useState ,useEffect} from 'react';
 
 export interface UserProfileProps {
     className?: string;
 }
 
-/**
- * This component was created using Codux's Default new component template.
- * To create custom component templates, see https://help.codux.com/kb/en/article/kb16522
- */
+
+interface UserData {
+  id: number;
+  username: string;
+  email: string;
+  // Add more fields as needed
+}
 export const UserProfile = ({ className }: UserProfileProps) => {
-    return <div className={classNames(styles.root, className)}>UserProfile11111111111</div>;
+    //const [userData, setUserData] = useState(null);
+    const [userData, setUserData] = useState<UserData | null>(null);
+    useEffect(() => {
+        const fetchData = async () => {
+        try {
+            const token = localStorage.getItem('token');
+            const response = await axios.get('https://zhiyouyuec.com/api/user/', {
+            headers: {
+                'Authorization': `Token ${token}`
+            }
+            });
+
+            if (response.status === 200) {
+            setUserData(response.data);
+            } else {
+            console.error('Failed to fetch user data. Unexpected status:', response.status);
+            }
+        } catch (error) {
+            console.error('Error fetching user data:', error);
+        }
+        };
+
+        fetchData();
+    }, []);
+
+    return (
+        <div>
+        {userData ? (
+            <div>
+            <h1>User Data</h1>
+            <p>ID: {userData.id}</p>
+            <p>Username: {userData.username}</p>
+            <p>Email: {userData.email}</p>
+            {/* Add more fields as needed */}
+            </div>
+        ) : (
+            <p>Loading user data...</p>
+        )}
+        </div>
+    );
 };
