8d9fc1850d4fe38bf258f344ccfd664dfee4a909
HankChoose <hankchenv@gmail.com>
2023-11-23 21:57:36 -0700

    Update views.py

--- backend/django_project/views.py
+++ backend/django_project/views.py
@@ -14,6 +14,20 @@ from django.shortcuts import HttpResponseRedirect, reverse
 from django.shortcuts import render
 
 
+def index(request):
+    '''
+    context = {
+        'STATIC_ROOT': settings.STATIC_ROOT,
+        'STATIC_URL': settings.STATIC_URL,
+    }
+
+
+    static_root = settings.STATIC_ROOT
+    static_url = settings.STATIC_URL
+    '''
+    return render(request, 'index.html')
+
+
 def pd(request):
     return render(request, 'pd.html')
 
