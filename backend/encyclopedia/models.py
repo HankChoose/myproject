df7b8be441d20b11fb687e450dd673c16b6eedf8
HankChoose <hankchenv@gmail.com>
2023-11-23 17:54:46 -0700

    2

--- backend/encyclopedia/models.py
+++ backend/encyclopedia/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+'''
 # Create your models here.
 
 class Book(models.Model):
@@ -9,7 +9,7 @@ class Book(models.Model):
 
     
 
-'''
+
 class Book(models.Model):
     title = models.CharField(max_length=100)
     author = models.CharField(max_length=100)
