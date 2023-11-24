962349de84ce5064ad8a521e49651282b1cc3ac4
HankChoose <hankchenv@gmail.com>
2023-11-23 22:05:48 -0700

    2

--- backend/django_project/models.py
+++ backend/django_project/models.py
@@ -5,3 +5,9 @@ class Book3(models.Model):
     title3 = models.CharField(max_length=100)
     author3 = models.CharField(max_length=100)
     publication_date = models.DateField()
+
+
+class Book4(models.Model):
+    title4 = models.CharField(max_length=100)
+    author4 = models.CharField(max_length=100)
+    publication_date = models.DateField()
--- backend/django_project/views.py
+++ backend/django_project/views.py
@@ -1,5 +1,6 @@
 
 from django.shortcuts import render
+from .models import Book3
 
 
 def index(request):
