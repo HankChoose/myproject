db9df95586b498123a5aef6d14af32e44111cf6a
HankChoose <hankchenv@gmail.com>
2023-11-25 01:35:59 -0700

    2

--- backend/django_project/testemail.py
+++ backend/django_project/testemail.py
@@ -1,18 +1,6 @@
-from django.contrib.sites.models import Site
 import smtplib
-import os
-from django import setup
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
-from django.contrib.sites.models import Site
-
-# 设置 DJANGO_SETTINGS_MODULE 环境变量
-os.environ['DJANGO_SETTINGS_MODULE'] = 'django_project.settings'  # 替换为你的项目名称
-# 初始化 Django
-setup()
-
-# 获取当前站点（通常 ID 为 1）
-site = Site.objects.get(id=1)
 
 # Your email configuration
 EMAIL_HOST = 'smtp.gmail.com'
@@ -20,13 +8,12 @@ EMAIL_PORT = 587
 EMAIL_HOST_USER = 'zhiyouyuec@gmail.com'
 EMAIL_HOST_PASSWORD = 'lawr wxds ovuc gpik'
 
-
 # Create a test message
 msg = MIMEMultipart()
 msg['From'] = 'zhiyouyuec@gmail.com'
 msg['To'] = 'hankchenv@gmail.com'
-msg['Subject'] = site.name + 'Hank:Test Subject'
-body = 'Hank-Test email body.'+site.name
+msg['Subject'] = 'Hank:Test Subject'
+body = 'Hank-Test email body.'
 msg.attach(MIMEText(body, 'plain'))
 
 # Try to send the email
