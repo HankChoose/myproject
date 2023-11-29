ed8e756953d41670cd18bf34431d9582646f0dd5
HankChoose <hankchenv@gmail.com>
2023-11-29 01:36:13 -0700

    2

--- frontend/ProjectVite01/src/components/user-profile/user-profile.tsx
+++ frontend/ProjectVite01/src/components/user-profile/user-profile.tsx
@@ -7,53 +7,57 @@ export interface UserProfileProps {
     className?: string;
 }
 
-
-interface UserData {
-  id: number;
-  username: string;
-  email: string;
-  // Add more fields as needed
-}
 export const UserProfile = ({ className }: UserProfileProps) => {
-    //const [userData, setUserData] = useState(null);
+    interface UserData {
+        username: string;
+        // 其他属性...
+    }
     const [userData, setUserData] = useState<UserData | null>(null);
     
     useEffect(() => {
-        const fetchData = async () => {
+        // 在组件加载时发送请求
+        fetchData();
+    }, []);
+
+    const fetchData = async () => {
+        // 获取保存在本地存储中的令牌
+        const token = localStorage.getItem('accessToken');
+
+        if (token) {
         try {
-            const token = localStorage.getItem('token');
-            const response = await axios.get('https://zhiyouyuec.com/user-profile/', {
+            const response = await fetch('hhttps://zhiyouyuec.com/user-profile/', {
+            method: 'GET',
             headers: {
-                'Authorization': `Token ${token}`
-            }
+                'Authorization': `Token ${token}`,  // 注意这里的格式，应为 `Token ${token}`
+                'Content-Type': 'application/json',
+            },
             });
 
-            if (response.status === 200) {
-                setUserData(response.data);
-                console.log(response.data);
+            if (response.ok) {
+            const data = await response.json();
+            setUserData(data);
             } else {
-            console.error('Failed to fetch user data. Unexpected status:', response.status);
+            // 处理请求失败的情况
+            console.error('Failed to fetch user data:', response.status, response.statusText);
             }
         } catch (error) {
             console.error('Error fetching user data:', error);
         }
-        };
-
-        fetchData();
-    }, []);
+        } else {
+        // 处理令牌不存在的情况
+        console.error('Access token is undefined or null.');
+        }
+    };
 
     return (
         <div>
         {userData ? (
             <div>
-            <h1>User Data</h1>
-            <p>ID: {userData.id}</p>
-            <p>Username: {userData.username}</p>
-            <p>Email: {userData.email}</p>
-            {/* Add more fields as needed */}
+            <p>Welcome, {userData.username}!</p>
+            {/* 其他用户信息的显示 */}
             </div>
         ) : (
-            <p>Loading user data...</p>
+            <p>Please log in to view user data.</p>
         )}
         </div>
     );
