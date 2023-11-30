ce57e4ef1d460e4af4429b5e04e7a5888303a8ea
HankChoose <hankchenv@gmail.com>
2023-11-30 01:41:15 -0700

    2

--- backend/django_project/views.py
+++ backend/django_project/views.py
@@ -48,8 +48,7 @@ def user_auth_token(request):
     return obtain_auth_token(request)
 
 
-@login_required
-@method_decorator(csrf_protect, name='dispatch')
+@method_decorator(csrf_protect, login_required, name='dispatch')
 class UserProfileView(APIView):
     def get(self, request):
         # user_email = request.user.email
