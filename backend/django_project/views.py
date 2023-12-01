030e7f17d5dbbcbfc096481af9205fa112e1b7a2
HankChoose <hankchenv@gmail.com>
2023-12-01 03:48:51 -0700

    2

--- backend/django_project/views.py
+++ backend/django_project/views.py
@@ -19,6 +19,7 @@ from rest_framework.authtoken.models import Token
 from rest_framework.authtoken.views import ObtainAuthToken, obtain_auth_token
 from rest_framework import generics
 from rest_framework.views import APIView
+from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import api_view, permission_classes, renderer_classes
@@ -154,6 +155,7 @@ def UserDemandCreateView2(request):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 @csrf_exempt
 def UserDemandCreateView3(request):
