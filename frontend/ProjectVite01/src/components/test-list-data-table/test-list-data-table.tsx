9d40aaa09e2c1ab4c626164255961f0698097dd4
HankChoose <hankchenv@gmail.com>
2023-12-04 19:35:41 -0700

    Update test-list-data-table.tsx

--- frontend/ProjectVite01/src/components/test-list-data-table/test-list-data-table.tsx
+++ frontend/ProjectVite01/src/components/test-list-data-table/test-list-data-table.tsx
@@ -19,7 +19,14 @@ interface Data {
     // 其他属性...
 }
 
-
+const sampleData = [
+    { id: '1', demand_type: 'demand_type 1', demand_description: 'Description 1', username: 'hank1', email: 'hank1@example.com' },
+    { id: '2', demand_type: 'demand_type 2', demand_description: 'Description 2', username: 'Ray2', email: 'ray2@example.com' },
+    { id: '3', demand_type: 'demand_type 3', demand_description: 'Description 3', username: 'hank1', email: 'hank1@example.com' },
+    { id: '4', demand_type: 'demand_type 4', demand_description: 'Description 4', username: 'Ray2', email: 'ray2@example.com' },
+    { id: '5', demand_type: 'demand_type 5', demand_description: 'Description 5', username: 'hank1', email: 'hank1@example.com' },
+    { id: '6', demand_type: 'demand_type 6', demand_description: 'Description 6', username: 'Ray2', email: 'ray2@example.com' },
+];
 
 export const TestListDataTable = ({ className }: TestListDataTableProps) => {
     
@@ -60,6 +67,6 @@ export const TestListDataTable = ({ className }: TestListDataTableProps) => {
     };
 
     return <div className={classNames(styles.root, className)}>
-        <TestDataTable data={data} />
+        <TestDataTable data={sampleData} />
     </div>;
 };
