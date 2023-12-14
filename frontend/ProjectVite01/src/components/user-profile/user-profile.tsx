f1380b539e2e3f5dc5b4141976e992e16c50aeb6
HankChoose <hankchenv@gmail.com>
2023-12-14 10:38:30 -0700

    Update user-profile.tsx

--- frontend/ProjectVite01/src/components/user-profile/user-profile.tsx
+++ frontend/ProjectVite01/src/components/user-profile/user-profile.tsx
@@ -3,6 +3,8 @@ import styles from './user-profile.module.scss';
 import axios from 'axios';
 import React, { useState ,useEffect} from 'react';
 import {baseUrl} from '../../constants';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Table from 'react-bootstrap/Table';
 
 export interface UserProfileProps {
     className?: string;
@@ -59,21 +61,55 @@ export const UserProfile = ({ className }: UserProfileProps) => {
             <div>
             <h1>User Home</h1>
             <p>Welcome!</p>
-            <ul>
-                {userData.map(item => (
-                <li key={item.id}>{item.username}</li>
-                ))}
-            </ul>
            
-            <ul>
-                {userData.map(item => (
-                <li key={item.id}>{item.email}</li>
-                ))}
-            </ul>
+           
+           
+
+            <Table bordered>
+            <thead>
+            <tr>
+            <th>Item</th>
+            <th>Detail</th>
+            </tr>
+            </thead>
+            <tbody>
+                <tr>
+                <td>Name:</td>
+                <td> 
+                <ul>
+                    {userData.map(item => (
+                    <li key={item.id}>{item.username}</li>
+                    ))}
+                </ul>
+               </td>
+                </tr>
+                <tr>
+                <td>Email:</td>
+                <td>
+                <ul>
+                    {userData.map(item => (
+                    <li key={item.id}>{item.email}</li>
+                    ))}
+                </ul>
+              </td>
+                </tr>
+                <tr>
+                <td>Type</td>
+                <td>111</td>
+                </tr>
+                <tr>
+                <td>Comment</td>
+                <td>wwww</td>
+                </tr>
+            </tbody>
+            </Table>
+
             </div>
         ) : (
             <p>Please log in to view user data.</p>
         )}
+
+         
         </div>
     );
 };
