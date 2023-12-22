480f8f20b662baa62007275ef3feae705c7fd876
HankChoose <hankchenv@gmail.com>
2023-12-22 12:32:22 -0700

    2

--- frontend/ProjectVite01/src/components/user-apply/user-apply.tsx
+++ frontend/ProjectVite01/src/components/user-apply/user-apply.tsx
@@ -6,7 +6,7 @@ import React, { useRef, useState, Component } from 'react';
 import { useSelector, useDispatch } from "react-redux";
 import { updateName, updateEmail } from "../../actions/userInfoActions";
 import { BrowserRouter as Router, Route, Routes, Link, useNavigate } from 'react-router-dom';
-import { baseUrl } from '../../constants';
+import {baseUrl} from '../../constants';
 import Form from 'react-bootstrap/Form';
 import Modal from 'react-modal';
 import { SignCard } from '../sign-card/sign-card';
@@ -25,26 +25,25 @@ type RootState = {
 
 // 在组件外部定义模态框的样式
 const modalStyles = {
-  overlay: {
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-        backgroundColor: 'rgba(203, 196, 223, 0.5)', // 背景颜色，可根据需要修改
-        
-        },
-  content: {
- 
-    top: 'auto',
-    left: 'auto',
-    right: 'auto',
-    bottom: 'auto',
-    //border: 'none', // 移除边框
-    //background: 'transparent', // 设定透明背景
-    padding: 0, // 移除默认 padding
-    borderRadius: 0, // 可以根据需要设置圆角
-  },
+    overlay: {
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            backgroundColor: 'rgba(203, 196, 223, 0.5)', // 背景颜色，可根据需要修改
+            
+            },
+    content: {
+    
+        top: 'auto',
+        left: 'auto',
+        right: 'auto',
+        bottom: 'auto',
+        //border: 'none', // 移除边框
+        //background: 'transparent', // 设定透明背景
+        padding: 0, // 移除默认 padding
+        borderRadius: 0, // 可以根据需要设置圆角
+    },
 };
-
 
 /**
  * This component was created using Codux's Default new component template.
@@ -54,8 +53,8 @@ export const UserApply = ({ className }: UserApplyProps) => {
 
     const userInfo = useSelector((state: RootState) => state.userInfo);
     const dispatch = useDispatch();
-
-    const [isModalOpen, setIsModalOpen] = useState(false);
+	
+	const [isModalOpen, setIsModalOpen] = useState(false);
 
     const openModal = () => {
         setIsModalOpen(true);
@@ -72,6 +71,7 @@ export const UserApply = ({ className }: UserApplyProps) => {
         closeModal();
     };
 
+
     const handleNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         dispatch(updateName(e.target.value));
         console.log("Name is:", e.target.value);
@@ -87,41 +87,40 @@ export const UserApply = ({ className }: UserApplyProps) => {
     };
 
 
-    return <div className={classNames(styles.root, className)}>
+    return 
+    <div className={classNames(styles.root, className)}>
         <div className={classNames(styles.flowImage)}></div>
         <div className={classNames(styles.FormRow)}></div>
-        <div className={styles.FromArea}>
+        <div className={styles.FromArea}> 
             <div className={classNames(styles.FormRow)}>
-                <Form.Control type="text" placeholder="Name" value={userInfo.name} onChange={handleNameChange} />
+                <Form.Control type="text"  placeholder="Name" value={userInfo.name} onChange={handleNameChange} /> 
             </div>
             <div className={classNames(styles.FormRow)}></div>
             <div className={classNames(styles.FormRow)}></div>
-
+             
             <div className={classNames(styles.FormRow)}>
                 <Form.Control type="text" placeholder="Email" value={userInfo.email} onChange={handleEmailChange} />
             </div>
             <div className={classNames(styles.FormRow)}></div>
             <div className={classNames(styles.FormRow)}></div>
             <div className={classNames(styles.FormRow)}>
-                <Link to="/react/userapply2"><Button variant="primary">Next page</Button>{' '}</Link>
-                <a href="https://zhiyouyuec.com"><Button variant="primary">Cancel</Button></a>
+            <Link to="/react/userapply2"><Button variant="primary">Next page</Button>{' '}</Link>
+            <a href="https://zhiyouyuec.com"><Button variant="primary">Cancel</Button></a>   
             </div>
         </div>
         <div>
             <button onClick={openModal}>提交需求</button>
-
             <Modal
                 isOpen={isModalOpen}
                 onRequestClose={closeModal}
                 contentLabel="登录模态框"
                 ariaHideApp={false}
                 shouldCloseOnOverlayClick={false}
-               className="custom-modal"
+                style={modalStyles} // 设置模态框的样式
             >
                 {/* 在模态框中渲染 Login 组件 */}
                 <SignCard onLogin={handleLogin} />
             </Modal>
- 
-        </div>
+        </div> 
     </div>;
 };
