d37775517d19ee8d5c6a20150af892586dec57de
HankChoose <hankchenv@gmail.com>
2023-12-21 22:28:18 -0700

    2

--- frontend/ProjectVite01/src/components/user-apply/user-apply.tsx
+++ frontend/ProjectVite01/src/components/user-apply/user-apply.tsx
@@ -6,9 +6,11 @@ import React, { useRef, useState, Component } from 'react';
 import { useSelector, useDispatch } from "react-redux";
 import { updateName, updateEmail } from "../../actions/userInfoActions";
 import { BrowserRouter as Router, Route, Routes, Link, useNavigate } from 'react-router-dom';
-import {baseUrl} from '../../constants';
+import { baseUrl } from '../../constants';
 import Form from 'react-bootstrap/Form';
 import Modal from 'react-modal';
+import { SignCard } from '../sign-card/sign-card';
+import { SignCardSmall } from '../sign-card-small/sign-card-small';
 
 export interface UserApplyProps {
     className?: string;
@@ -29,7 +31,7 @@ export const UserApply = ({ className }: UserApplyProps) => {
 
     const userInfo = useSelector((state: RootState) => state.userInfo);
     const dispatch = useDispatch();
-    
+
     const [isModalOpen, setIsModalOpen] = useState(false);
 
     const openModal = () => {
@@ -40,9 +42,9 @@ export const UserApply = ({ className }: UserApplyProps) => {
         setIsModalOpen(false);
     };
 
-    const handleLogin = () => {
-        // 处理登录逻辑
-        console.log('处理登录逻辑');
+    const handleLogin = (username: string, password: string) => {
+        // 在这里处理登录逻辑，可以向服务器发送请求等
+        console.log('登录成功', username);
         // 在登录成功后，关闭模态框
         closeModal();
     };
@@ -65,47 +67,35 @@ export const UserApply = ({ className }: UserApplyProps) => {
     return <div className={classNames(styles.root, className)}>
         <div className={classNames(styles.flowImage)}></div>
         <div className={classNames(styles.FormRow)}></div>
-        <div className={styles.FromArea}> 
+        <div className={styles.FromArea}>
             <div className={classNames(styles.FormRow)}>
-                <Form.Control type="text"  placeholder="Name" value={userInfo.name} onChange={handleNameChange} /> 
+                <Form.Control type="text" placeholder="Name" value={userInfo.name} onChange={handleNameChange} />
             </div>
             <div className={classNames(styles.FormRow)}></div>
             <div className={classNames(styles.FormRow)}></div>
-             
+
             <div className={classNames(styles.FormRow)}>
                 <Form.Control type="text" placeholder="Email" value={userInfo.email} onChange={handleEmailChange} />
             </div>
             <div className={classNames(styles.FormRow)}></div>
             <div className={classNames(styles.FormRow)}></div>
             <div className={classNames(styles.FormRow)}>
-            <Link to="/react/userapply2"><Button variant="primary">Next page</Button>{' '}</Link>
-            <a href="https://zhiyouyuec.com"><Button variant="primary">Cancel</Button></a>   
+                <Link to="/react/userapply2"><Button variant="primary">Next page</Button>{' '}</Link>
+                <a href="https://zhiyouyuec.com"><Button variant="primary">Cancel</Button></a>
             </div>
         </div>
-         <div>
-      <button onClick={openModal}>提交需求</button>
+        <div>
+            <button onClick={openModal}>提交需求</button>
 
-      <Modal
-        isOpen={isModalOpen}
-        onRequestClose={closeModal}
-        contentLabel="登录模态框"
-        >
-            <h2>登录</h2>
-            {/* 添加你的登录表单和逻辑 */}
-            <form>
-            {/* 输入用户名和密码的表单元素 */}
-            <label>
-                用户名:
-                <input type="text" />
-            </label>
-            <label>
-                密码:
-                <input type="password" />
-            </label>
-            {/* 登录按钮 */}
-            <button onClick={handleLogin}>登录</button>
-            </form>
-        </Modal>
+            <Modal
+                isOpen={isModalOpen}
+                onRequestClose={closeModal}
+                contentLabel="登录模态框"
+            >
+                {/* 在模态框中渲染 Login 组件 */}
+                <SignCardSmall onLogin={handleLogin} />
+            </Modal>
+ 
         </div>
     </div>;
 };
