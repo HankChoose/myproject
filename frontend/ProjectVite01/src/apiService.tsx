749e3a81083c3ddbb02f27f871753153c3004518
HankChoose <hankchenv@gmail.com>
2023-12-25 10:58:06 -0700

    2

--- frontend/ProjectVite01/src/apiService.tsx
+++ frontend/ProjectVite01/src/apiService.tsx
@@ -13,8 +13,10 @@ const handleError = (error:any) => {
   console.error('API Request Error:', error);
   throw error;
 };
+
 /////////////////////////////////////////////////fetch_data_token_get for /user-profile/
 const fetch_data_token_get = async (url:string,token:any) => {
+  const console_title='fetch_data_token_get';
   if (token) {
       const config_fetch_data_token_get = {
         method: 'GET',
@@ -27,28 +29,69 @@ const fetch_data_token_get = async (url:string,token:any) => {
         const response = await fetch(`${baseUrl}${url}`, config_fetch_data_token_get);
         if (response.ok) {
             const data = await response.json();
-            console.log('fetch_data_token_get response data',data);
+            console.log(console_title+'response data',data);
             return data;
 
         } else {
           // 处理请求失败的情况
-          console.error('fetch_data_token_get response data error:', response.status, response.statusText);
-          return handleError('fetch_data_token_get response data error:'+response.status +response.statusText);
+          console.error(console_title+' response data error:', response.status, response.statusText);
+          return handleError(console_title+' response data error:'+response.status +response.statusText);
          
         }
         
       } catch (error) {
-          console.error('fetch_data_token_get error:', error);
-          return handleError('fetch_data_token_get error:'+ error);
+          console.error(console_title+' error:', error);
+          return handleError(console_title+' error:'+ error);
          
       }
     } else {
       // 处理令牌不存在的情况
-      console.error('fetch_data_token_get token is undefined or null');
-      return handleError('fetch_data_token_get token is undefined or null');
+      console.error(console_title+' token is undefined or null');
+      return handleError(console_title+' token is undefined or null');
      
     }
 
 };
 
-export { fetch_data_token_get };
+
+/////////////////////////////////////////////////fetch_data_token_get for /user-change-username/
+const fetch_data_token_post = async (url:string,token:any,username:string) => { 
+  const console_title='fetch_data_token_post';
+  if (token) {
+      const config_fetch_data_token_post = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Token ${token}`,
+            },
+        body: JSON.stringify({ new_username: username }),
+      };
+      try {
+        const response = await fetch(`${baseUrl}${url}`, config_fetch_data_token_post);
+        if (response.ok) {
+            const data = await response.json();
+            console.log(console_title+ 'response data',data);
+            return data;
+
+        } else {
+          // 处理请求失败的情况
+          console.error(console_title+ ' response data error:', response.status, response.statusText);
+          return handleError(console_title+ ' response data error:'+response.status +response.statusText);
+         
+        }
+        
+      } catch (error) {
+          console.error(console_title+ ' error:', error);
+          return handleError(console_title+ ' error:'+ error);
+         
+      }
+    } else {
+      // 处理令牌不存在的情况
+      console.error(console_title+ ' token is undefined or null');
+      return handleError(console_title+ ' token is undefined or null');
+     
+    }
+
+};
+
+export { fetch_data_token_get,fetch_data_token_post };
--- frontend/ProjectVite01/src/components/user-apply/user-apply.tsx
+++ frontend/ProjectVite01/src/components/user-apply/user-apply.tsx
@@ -46,7 +46,6 @@ const modalStyles = {
     },
 };
 
-
 /**
  * This component was created using Codux's Default new component template.
  * To create custom component templates, see https://help.codux.com/kb/en/article/kb16522
