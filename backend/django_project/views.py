74cac21b9dcbb9a4bfc592527698bb90bbcf1a68
HankChoose <hankchenv@gmail.com>
2024-01-05 15:20:37 -0700

    2

--- backend/django_project/views.py
+++ backend/django_project/views.py
@@ -20,9 +20,10 @@ from django.utils.decorators import method_decorator
 from django.http import HttpResponseServerError, HttpResponse
 from django.db import connection
 from django.conf import settings
-from django.views.decorators.csrf import csrf_exempt, csrf_protect
+from django.views.decorators.csrf import csrf_exempt, csrf_protect, require_POST
 from django.views.generic import TemplateView
 from django.shortcuts import get_object_or_404
+
 
 from rest_framework.authtoken.models import Token
 from rest_framework.authtoken.views import ObtainAuthToken, obtain_auth_token
@@ -186,23 +187,32 @@ class CheckUserAPIView(APIView):
 
 
 # -------------------------------------------->For UserApply
+@require_POST
 def upload_user_apply(request):
-    username = request.POST.get('username')
-    email = request.POST.get('email')
-    applytype = request.POST.get('applytype')
-    requirements = request.POST.get('requirements')
-    mainImageId = request.POST.get('mainImageId')
-    # 处理其他信息保存到数据库
-    user_apply = UserApply.objects.create(
-        username=username,
-        email=email,
-        apply_type=applytype,
-        requirements=requirements,
-        main_image_id=mainImageId
-    )
+    # 获取文本数据
+    username = request.POST.get('username', '')
+    email = request.POST.get('email', '')
+    applytype = request.POST.get('applytype', '')
+    requirements = request.POST.get('requirements', '')
 
-    # 返回主图 ID
-    return Response({'message': 'Data uploaded successfully', 'username': username, 'email': email, 'applytype': applytype, 'requirements': requirements, 'mainImageId': mainImageId})
+    # 处理其他文本数据的逻辑...
+
+    # 获取文件数据
+    # 'uploadedImages'应该与前端formData.append的键名一致
+    uploaded_images = request.FILES.getlist('uploadedImages')
+
+    for idx, uploaded_image in enumerate(uploaded_images, start=1):
+        # 处理每个上传的文件
+        file_name = f"{username}_image_{idx}.jpg"  # 生成唯一的文件名
+        save_path = os.path.join(settings.MEDIA_ROOT, 'uploads', file_name)
+
+        with open(save_path, 'wb') as destination:
+            for chunk in uploaded_image.chunks():
+                destination.write(chunk)
+
+        # 处理文件上传后的逻辑，例如将文件路径保存到数据库
+
+    return JsonResponse({'message': 'Data uploaded successfully'})
 
 
 class UserApplyCreateView(generics.CreateAPIView):
