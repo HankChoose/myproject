92240d4f5b0117232f472b921556cf0d09b74c93
HankChoose <hankchenv@gmail.com>
2024-01-05 01:09:32 -0700

    2

--- frontend/ProjectVite01/src/_codux/boards/test-list-component/test-list-component.board.tsx
+++
@@ -1,8 +0,0 @@
-import { createBoard } from '@wixc3/react-board';
-import TestListComponent  from '../../../components/test-list-component/test-list-component';
-
-export default createBoard({
-    name: 'TestListComponent',
-    Board: () => <TestListComponent />,
-    isSnippet: true,
-});
