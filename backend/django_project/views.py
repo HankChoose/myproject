47d0b59b5508cc0d915e84d0a6f2a7a4a0ba1965
HankChoose <hankchenv@gmail.com>
2024-01-08 14:31:28 -0700

    Update views.py

--- backend/django_project/views.py
+++ backend/django_project/views.py
@@ -52,7 +52,7 @@ def clean_filename(filename):
 
 def get_image(request, image_info):
     # 构建图片的完整路径
-    image_path = f'/uploads/{image_info}'
+    image_path = f'uploads/{image_info}'
     full_path = os.path.join(settings.MEDIA_ROOT, image_path)
 
     return JsonResponse({'full_path': full_path})
