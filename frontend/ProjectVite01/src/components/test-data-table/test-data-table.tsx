723a0f01895125c7e93a53be4410ed5d89828c8b
HankChoose <hankchenv@gmail.com>
2024-01-12 10:31:06 -0700

    2

--- frontend/ProjectVite01/src/components/test-data-table/test-data-table.tsx
+++ frontend/ProjectVite01/src/components/test-data-table/test-data-table.tsx
@@ -113,9 +113,7 @@ export const TestDataTable = ({ className, data }: TestDataTableProps) => {
             <tr key={item.id}>
                 <td>{item.id}</td>
                 <td>
-                    <a href={`userapplycontent/${item.id}`} target="_self" rel="noopener noreferrer">
-                            <TestGetImages imageInfo={item.image_path_main} />
-                    </a>
+                    <TestGetImages imageInfo={item.image_path_main} />
                 </td>
                 <td>
                     <a href={`userapplycontent/${item.id}`} target="_self" rel="noopener noreferrer">
