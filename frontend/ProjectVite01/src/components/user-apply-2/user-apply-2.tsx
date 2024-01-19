047ce8e0b62ba500592a2a214812f1234b148d33
HankChoose <hankchenv@gmail.com>
2024-01-19 02:02:02 -0700

    Update user-apply-2.tsx

--- frontend/ProjectVite01/src/components/user-apply-2/user-apply-2.tsx
+++ frontend/ProjectVite01/src/components/user-apply-2/user-apply-2.tsx
@@ -310,16 +310,16 @@ export const UserApply2 = ({ className }: UserApply2Props) => {
                         <option value="Flask">Flask</option>
                         <option value="FastAPI">FastAPI</option>
                         <option value="Flutter">Flutter</option>
-                        <option value="React">React</option>
-                        <option value="SpringBoot">SpringBoot</option>
-                        <option value="Nodejs">Nodejs</option>
-                        <option value="WebProgramming">WebProgramming</option>
-                        <option value="REST">REST</option>
-                        <option value="SQL">SQL</option>
-                        <option value="NoSQL">NoSQL</option>
-                        <option value="Unix">Unix</option>
                         <option value="Linux">Linux</option>
+                        <option value="Nodejs">Nodejs</option>
+                        <option value="NoSQL">NoSQL</option>
+                        <option value="REST">REST</option>
+                        <option value="React">React</option>
                         <option value="Ruby">Ruby</option>
+                        <option value="SQL">SQL</option>
+                        <option value="SpringBoot">SpringBoot</option>
+                        <option value="Unix">Unix</option>
+                        <option value="WebProgramming">WebProgramming</option>
                         <option value="Others">Others</option>
 
                        
