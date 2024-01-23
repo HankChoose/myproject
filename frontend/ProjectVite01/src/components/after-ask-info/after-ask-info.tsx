3bad5a8b7042b8c5613025afdfd4931508aa5d8a
HankChoose <hankchenv@gmail.com>
2024-01-23 08:15:03 -0700

    2

--- frontend/ProjectVite01/src/components/after-ask-info/after-ask-info.tsx
+++ frontend/ProjectVite01/src/components/after-ask-info/after-ask-info.tsx
@@ -1,5 +1,6 @@
 import classNames from 'classnames';
 import styles from './after-ask-info.module.scss';
+import { Button, Card, Table, ListGroup } from 'react-bootstrap';
 
 export interface AfterAskInfoProps {
     className?: string;
@@ -12,8 +13,17 @@ export interface AfterAskInfoProps {
 export const AfterAskInfo = ({ className }: AfterAskInfoProps) => {
     const aferaskinfo="The message has been sent successfully, return to ";
     return <div className={classNames(styles.root, className)}>
-        <div className={classNames(styles.FormRow)}></div>
-        <div className={classNames(styles.FormRow)}></div>
-        {aferaskinfo}<a href="https://zhiyouyuec.com">Home</a>
+            <Card style={{ width: '60vw' }}>
+                <Card.Body>
+                    <Card.Title>
+                        <h2>Message has been sent</h2>
+                    </Card.Title>
+                    <Card.Text></Card.Text>
+                </Card.Body>
+                
+                <Card.Body>
+                    {aferaskinfo}<a href="https://zhiyouyuec.com">Home</a>
+                </Card.Body>
+            </Card>
         </div>;
 };
