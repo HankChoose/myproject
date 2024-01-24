39e68636f21dd71e48a4e897c9649b10b1c9ccb9
HankChoose <hankchenv@gmail.com>
2024-01-24 05:54:06 -0700

    3

--- frontend/ProjectVite01/src/_codux/boards/contact-us/contact-us.board.tsx
+++ frontend/ProjectVite01/src/_codux/boards/contact-us/contact-us.board.tsx
@@ -1,8 +1,9 @@
 import { createBoard } from '@wixc3/react-board';
 import { ContactUs } from '../../../components/contact-us/contact-us';
+import { BrowserRouter as Router, Route, Routes, Link, useNavigate } from 'react-router-dom';
 
 export default createBoard({
     name: 'ContactUs',
-    Board: () => <ContactUs />,
+    Board: () => <Router><ContactUs /></Router>,
     isSnippet: true,
 });
