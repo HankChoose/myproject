5d825919c0d0ea85eaf620619cd751dc47116914
HankChoose <hankchenv@gmail.com>
2025-04-22 08:34:32 -0600

    2

--- frontend/ProjectVite01/src/constants.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.isLogErrorVisible = exports.isLogVisible = exports.baseUrl = void 0;
-// constants.js
-exports.baseUrl = "https://zhiyouyuec.com";
-exports.isLogVisible = false;
-exports.isLogErrorVisible = true;
