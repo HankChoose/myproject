c07390c29db9efaa26dc3b6498bddb4bfe97ad20
HankChoose <hankchenv@gmail.com>
2025-04-22 08:56:30 -0600

    Update vite.config.ts

--- frontend/ProjectVite01/vite.config.ts
+++ frontend/ProjectVite01/vite.config.ts
@@ -2,10 +2,9 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
 
-export default defineConfig({
+eexport default defineConfig({
   plugins: [react(), svgr()],
   esbuild: {
-    loader: 'jsx',                // 👈 告诉 Vite 用 JSX 方式加载
-    include: /src\/.*\.js$/,      // 👈 仅针对 src 下的 .js 文件
+    loader: { '.js': 'jsx' }, // 全局开启 `.js` 的 JSX 支持
   },
 });
