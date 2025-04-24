bde5be520a457cb4137359d506feaecbd06db3de
HankChoose <hankchenv@gmail.com>
2025-04-24 00:22:16 -0600

    2

--- frontend/ProjectVite01/vite.config.ts
+++ frontend/ProjectVite01/vite.config.ts
@@ -2,7 +2,10 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react(), svgr()],
+  plugins: [react(), svgr()],
+  server: {
+    host: true,     // 允许从外部访问，比如远程服务器IP或域名
+    port: 3000,     // 自定义端口为 3000
+  },
 });
