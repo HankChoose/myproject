f6e856e2da35e1d78602f624f66484386dd55404
HankChoose <hankchenv@gmail.com>
2025-04-24 00:47:38 -0600

    Update vite.config.ts

--- frontend/ProjectVite01/vite.config.ts
+++ frontend/ProjectVite01/vite.config.ts
@@ -8,5 +8,6 @@ export default defineConfig({
     host: true,     // 允许从外部访问，比如远程服务器IP或域名
     port: 3000,     // 自定义端口为 3000
 	allowedHosts: ['localhost', 'zhiyouyuec.com'],
+	base: '/react/',
   },
 });
