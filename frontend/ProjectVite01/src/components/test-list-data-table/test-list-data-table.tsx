d96210a0c9744c1797bc41ac1e349ca680616e7e
HankChoose <hankchenv@gmail.com>
2025-04-24 02:15:48 -0600

    Update test-list-data-table.tsx

--- frontend/ProjectVite01/src/components/test-list-data-table/test-list-data-table.tsx
+++ frontend/ProjectVite01/src/components/test-list-data-table/test-list-data-table.tsx
@@ -91,5 +91,6 @@ export const TestListDataTable = ({ className }: TestListDataTableProps) => {
                     </button>
                    
                 </div>
+				{ TestDataTable }
         </div>
 };
