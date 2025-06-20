9d411820e60ec2e3401d83564d64d88683e409b5
Hank Chen <106391093+HankChoose@users.noreply.github.com>
2025-06-20 02:20:19 -0600

    Update settings.py

--- backend/django_project/settings.py
+++ backend/django_project/settings.py
@@ -194,7 +194,6 @@ SOCIALACCOUNT_AUTO_SIGNUP = True
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'corsheaders.middleware.CorsMiddleware',
-    'allauth.account.middleware.AccountMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
