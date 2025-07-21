60262832d35ff9782dd27aac3d6030d7ff1cc517
Hank Chen <106391093+HankChoose@users.noreply.github.com>
2025-07-21 16:12:48 -0600

    2

--- backend/django_project/settings.py
+++ backend/django_project/settings.py
@@ -298,6 +298,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/dev/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / "static"]
 # STATICFILES_DIRS = [os.path.join(BASE_DIR, 'frontend', 'build', 'static')]
 
 # Default primary key field type
