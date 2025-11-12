b000fb3d5477034a69b28129ac1f9207b44c8eb7
Hank Chen <106391093+HankChoose@users.noreply.github.com>
2025-11-11 21:32:24 -0700

    Update vite.config.js

--- office-demo/src/ProjectVite01/vite.config.js
+++ office-demo/src/ProjectVite01/vite.config.js
@@ -1,13 +1,21 @@
-const { defineConfig } = require('vite')
-const react = require('@vitejs/plugin-react')
-const svgr = require('vite-plugin-svgr')
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const { defineConfig } = require("vite");
+const react = __importDefault(require("@vitejs/plugin-react"));
+const svgr = __importDefault(require("vite-plugin-svgr"));
 
 // https://vitejs.dev/config/
 module.exports = defineConfig({
-  plugins: [react(), svgr()],
-  server: {
-    host: '0.0.0.0',
-    port: 3000,
-    allowedHosts: ['hankchenv.com', 'www.hankchenv.com'],
-  },
-})
+    plugins: [react.default(), svgr.default()],
+    server: {
+        host: '0.0.0.0', // 接受所有外部访问
+        port: 3000,
+        allowedHosts: [
+            'hankchenv.com',
+            'www.hankchenv.com'
+        ],
+    },
+});
